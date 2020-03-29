43806d6a8dc38cf56a03d8294fda6d34374a8ade
Otaviano Mendes <otaviano.lm@gmail.com>
2020-03-28 23:44:15 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './src/app/header/header.component';
+import { Section1Component } from './src/app/section1/section1.component';
+import { Section2Component } from './src/app/section2/section2.component';
+import { FooterComponent } from './src/app/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    Section1Component,
+    Section2Component,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/src/app/section1/section1.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-section1',
+  templateUrl: './section1.component.html',
+  styleUrls: ['./section1.component.css']
+})
+export class Section1Component  {
+
+title = 'Historíco';
+
+
+}
